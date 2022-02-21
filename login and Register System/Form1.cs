f6620e9dd82a144b0be74df344165760df79209b
Marin Andreev <marinandreev85@gmail.com>
2022-02-21 09:29:33 +0200

    Add project files.

---
+++ login and Register System/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace login_and_Register_System
+{
+    public partial class frmRegister : Form
+    {
+        public frmRegister()
+        {
+            InitializeComponent();
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (txtUsername.Text == "" && txtpassword.Text == "" && txtComPassword.Text == "")
+            {
+                MessageBox.Show("Username and Password fields are empty", "Registration Failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+            else if (txtpassword.Text == txtComPassword.Text)
+            {
+                new EmailVerification(txtUsername.Text, txtpassword.Text).Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Passwords does not match, Please Re-enter", "Registration Failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtpassword.Text = "";
+                txtComPassword.Text = "";
+                txtpassword.Focus();
+            }
+        }
+
+        private void CheckbxShowPas_CheckedChanged(object sender, EventArgs e)
+        {
+            if (CheckbxShowPas.Checked)
+            {
+                txtpassword.PasswordChar = '\0';
+                txtComPassword.PasswordChar = '\0';
+            }
+            else
+            {
+                txtpassword.PasswordChar = '•';
+                txtComPassword.PasswordChar = '•';
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            txtUsername.Text = "";
+            txtpassword.Text = "";
+            txtComPassword.Text = "";
+            txtUsername.Focus();
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+            new frmLogin().Show();
+            this.Hide();
+        }
+    }
+}
+        
+  
